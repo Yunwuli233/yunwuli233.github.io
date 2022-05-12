22ae4ff70763493e70801fbcc534c604f7e704a7
Ayun <2719600151@qq.com>
2022-05-12 11:54:02 +0800

    Site updated: 2022-05-12 11:54:00

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -158,16 +158,17 @@ function loadWidget(config) {
 	}
 
 	(function initModel() {
-		// let modelId = localStorage.getItem("modelId"),
-		// 	modelTexturesId = localStorage.getItem("modelTexturesId");
-		// if (modelId === null) {
-		// 	// 首次访问加载 指定模型 的 指定材质
-		// 	let modelId = 3; // 模型 ID
-		// 	modelTexturesId = 0; // 材质 ID
-		// }
-		let	modelId = 3; // 模型 ID
+		let modelId = localStorage.getItem("modelId"),
+			modelTexturesId = localStorage.getItem("modelTexturesId");
+		if (modelId === null) {
+			// 首次访问加载 指定模型 的 指定材质
+			let modelId = 3; // 模型 ID
+			modelTexturesId = 0; // 材质 ID
+		}
+		let	model0Id = 3; // 模型 ID
 		modelTexturesId = 0; // 材质 ID
-		console.log(modelId,modelTexturesId);
+		//通过控制台输出当前看板娘的模型ID和材质ID
+		console.log("模型ID:"+modelId,"材质ID:"+modelTexturesId);
 		loadModel(modelId, modelTexturesId);
 		fetch(waifuPath)
 			.then(response => response.json())
